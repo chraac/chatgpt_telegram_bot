680b8f374676a4e4d31142826a0c1cb8665656c7
hongruichen <chraac@gmail.com>
2023-03-04 10:46:00 +0800

    add config to suppress network error

--- bot/config.py
+++ bot/config.py
@@ -19,6 +19,7 @@ allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
 mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
 sqlite_database_uri = config_env['SQLITE_DATABASE_PATH']
+ignore_network_error = config_yaml.get("ignore_network_error", False)
 
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:
